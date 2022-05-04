cb1040e4c427df638e239aa5ebb5f371fa4e4aec
MaxALEau <maxaleau@gmail.com>
2022-05-04 20:39:38 +0200

    auth

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import tweepy
-auth = tweepy.OAuthHandler('o3jvA5UO9Nc9daIA23jBq0uRn', '71Qw5glXJM3gtCdLbYHQkJWPThCxnsibd3ZStPNAyqtlWy61aV')
+auth = tweepy.OAuthHandler("yD0PYw6sHKXcfVblNsifwefP5","B1m4rUxhq64Z6vE2yKSi6PCV4RgDwxq4tvIH1Ll0ZlG5OE07TC")
+auth.set_access_token("1521864363820392454-Wkh8cpRgIlFVf61QY1QiJYVGreW9eY","F3TeeDIUAd4avD0lUYCThH8Akp4jo74ZMKkH2Ds7iZhum")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
